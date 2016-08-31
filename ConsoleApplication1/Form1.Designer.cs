07ab00cf2fe5f90aa78ee698bbaaecb4cc91b003
Toshiyuki Suzumura <suz.labo@smoche.sakuraweb.com>
2016-08-31 20:07:03 +0900

    initial commit

---
+++ ConsoleApplication1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+internal static class UsbNotification
+{
+    public const int DbtDevicearrival = 0x8000; // system detected a new device        
+    public const int DbtDeviceremovecomplete = 0x8004; // device is gone      
+    public const int WmDevicechange = 0x0219; // device change event      
+    private const int DbtDevtypDeviceinterface = 5;
+    private static readonly Guid GuidDevinterfaceUSBDevice = new Guid("A5DCBF10-6530-11D2-901F-00C04FB951ED"); // USB devices
+    private static IntPtr notificationHandle;
+
+    /// <summary>
+    /// Registers a window to receive notifications when USB devices are plugged or unplugged.
+    /// </summary>
+    /// <param name="windowHandle">Handle to the window receiving notifications.</param>
+    public static void RegisterUsbDeviceNotification(IntPtr windowHandle)
+    {
+        DevBroadcastDeviceinterface dbi = new DevBroadcastDeviceinterface
+        {
+            DeviceType = DbtDevtypDeviceinterface,
+            Reserved = 0,
+            ClassGuid = GuidDevinterfaceUSBDevice,
+            Name = 0
+        };
+
+        dbi.Size = Marshal.SizeOf(dbi);
+        IntPtr buffer = Marshal.AllocHGlobal(dbi.Size);
+        Marshal.StructureToPtr(dbi, buffer, true);
+
+        notificationHandle = RegisterDeviceNotification(windowHandle, buffer, 0);
+    }
+
+    /// <summary>
+    /// Unregisters the window for USB device notifications
+    /// </summary>
+    public static void UnregisterUsbDeviceNotification()
+    {
+        UnregisterDeviceNotification(notificationHandle);
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr RegisterDeviceNotification(IntPtr recipient, IntPtr notificationFilter, int flags);
+
+    [DllImport("user32.dll")]
+    private static extern bool UnregisterDeviceNotification(IntPtr handle);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct DevBroadcastDeviceinterface
+    {
+        internal int Size;
+        internal int DeviceType;
+        internal int Reserved;
+        internal Guid ClassGuid;
+        internal short Name;
+    }
+}
+
+
+namespace ConsoleApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public Action onUsbDeviceAdded { get; set; }
+        public Action onUsbDeviceRemoved { get; set; }
+
+
+        public void enableUsbDeviceNotification(Action onAdded, Action onRemoved)
+        {
+            UsbNotification.RegisterUsbDeviceNotification(this.Handle);
+            onUsbDeviceAdded = onAdded;
+            onUsbDeviceRemoved = onRemoved;
+        }
+
+
+        public void unregisterUsbDeviceEvent()
+        {
+            UsbNotification.UnregisterUsbDeviceNotification();
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == UsbNotification.WmDevicechange)
+            {
+                switch ((int)m.WParam)
+                {
+                    case UsbNotification.DbtDevicearrival:
+                        onUsbDeviceAdded();
+                        break;
+                    case UsbNotification.DbtDeviceremovecomplete:
+                        onUsbDeviceRemoved();
+                        break;
+                }
+            }
+        }   
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(173, 55);
+            this.ControlBox = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 form1 = new Form1();
+            form1.enableUsbDeviceNotification(() =>
+            {
+                Console.Error.WriteLine("Added");
+            }, () =>
+            {
+                Console.Error.WriteLine("Added");
+            });
+
+            Thread interfaceThread = new Thread((Object obj) =>
+            {
+                String msg;
+                while ((msg = Console.In.ReadLine()) != null)
+                {
+                    Console.Out.WriteLine(">>" + msg);
+                }
+                form1.unregisterUsbDeviceEvent();
+                Application.Exit();
+            });
+
+            interfaceThread.Start();
+            Application.Run(form1);
+        }
+    }
+}
